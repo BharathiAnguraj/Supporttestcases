d1d389c3444346f39d02ab2a3f59e3ca9c40f3fb
bharathi.a <bharathi.a@LT-LZ-0220.ivygdcad.ivy-support.com>
2023-06-22 11:35:25 +0530

    final commit 2

--- Test Suites/Support team cases.ts
+++ Test Suites/Support team cases.ts
@@ -15,7 +15,7 @@
       <guid>33217e93-c179-4a7d-b102-f3e9daceebfd</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Support team Case Flow</testCaseId>
+      <testCaseId>Test Cases/Mobile Scripts</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>

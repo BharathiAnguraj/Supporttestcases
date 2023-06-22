095b5d78c2b26ee76718e51563584afecbf13b8f
surendran.m <surendran.m@IVY-CRLSE-1034.ivygdcad.ivy-support.com>
2023-06-22 16:29:45 +0530

    Datepicker Locators

---
+++ Object Repository/Web/Sales Order View/select_SO Status.rs
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>select_SO Status</name>
+   <tag></tag>
+   <elementGuidId>93565e66-774c-4fa9-9378-e04ed3bc6674</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//li[contains(.,'Opened')]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ref_element</name>
+      <type>Main</type>
+      <value>Object Repository/Web/Frame/Main Frame</value>
+      <webElementGuid>50951cab-b25a-4ffa-a13d-0c38bf903203</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>

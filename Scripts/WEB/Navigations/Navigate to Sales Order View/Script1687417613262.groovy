aaf12fe5c8c49d15f812776756593fb31484e08b
bharathi.a <bharathi.a@LT-LZ-0220.ivygdcad.ivy-support.com>
2023-06-22 14:05:53 +0530

    Navigation Scripts

--- Scripts/WEB/Navigations/Navigate to Sales Order View/Script1687417613262.groovy
+++ Scripts/WEB/Navigations/Navigate to Sales Order View/Script1687417613262.groovy
@@ -17,3 +17,6 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+WebUI.callTestCase(findTestCase('WEB/Navigations/Re_Usables/IVYNavigation'), [('Navigation') : 'Transactions;Receivables;Collections;Collections'], 
+    FailureHandling.STOP_ON_FAILURE)
+

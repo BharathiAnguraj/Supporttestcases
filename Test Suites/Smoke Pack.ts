3b4d4ac2662863e68e2c089a15d458554287f085
bharathi.a <bharathi.a@LT-LZ-0220.ivygdcad.ivy-support.com>
2023-06-30 21:17:09 +0530

    file removal

--- Test Suites/Smoke Pack.ts
+++ Test Suites/Smoke Pack.ts
@@ -14,7 +14,7 @@
    <testCaseLink>
       <guid>33217e93-c179-4a7d-b102-f3e9daceebfd</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Mobile Scripts</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
